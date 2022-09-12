554ca86dc73b7d8efa5d7a948353cf4cfb2d580c
mirax87 <michi.rauer@googlemail.com>
2022-09-12 16:29:07 +0200

    doc: STAR_chimeric/README.md

--- STAR_Chimeric/run_snakemake.sh
+++ STAR_Chimeric/run_snakemake.sh
@@ -3,4 +3,4 @@ module load slurm snakemake R/4.0.0 sambamba
 
 read workdir configyaml params <<< "$@"
 
-snakemake -d ${workdir} ${params} --configfile $configyaml --cluster "SlurmEasy -n {rule} -t {threads} -m 8G -l cluster_log" --latency-wait 180 --keep-going
+snakemake -d ${workdir} ${params} --configfile $configyaml 
