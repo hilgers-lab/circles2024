9256d0e0b9bae9835e41b3b6bc47f0ccea5a25d5
mirax87 <michi.rauer@googlemail.com>
2022-09-12 16:38:38 +0200

    Fail safe for user setup: snakemake.CIRIquant

--- snakemake.CIRIquant/run_snakemake.sh
+++ snakemake.CIRIquant/run_snakemake.sh
@@ -8,6 +8,9 @@ read workdir circles fastq_dir samplelist_dir params <<< "$@"
 
 # Update your <conda prefix>
 conda="--use-conda --conda-prefix <conda prefix>"
+echo "Update your <conda prefix>"
+exit
+
 config="samplelist_dir=$(realpath $samplelist_dir) fastq_directory=$(realpath $fastq_dir) circles=$(realpath $circles)"
 snakemakeconfig="$config $params $conda $cluster"
 snakemake --snakefile Snakefile --directory $workdir --configfile "genome.yaml" --config $snakemakeconfig
