07ef0e141912112c36717b088c6dc7940e958788
mirax87 <michi.rauer@googlemail.com>
2022-09-12 16:32:39 +0200

    Clean code: snakemake.CIRIquant

--- snakemake.CIRIquant/run_snakemake.sh
+++ snakemake.CIRIquant/run_snakemake.sh
@@ -6,8 +6,8 @@ read workdir circles fastq_dir samplelist_dir params <<< "$@"
 
 [ ! -d "$(realpath $fastq_dir)" ] && { echo "Required input fastq dir. Exit..."; exit; }
 
+# Update your <conda prefix>
 conda="--use-conda --conda-prefix <conda prefix>"
-cluster=" --latency-wait 180 --keep-going"
 config="samplelist_dir=$(realpath $samplelist_dir) fastq_directory=$(realpath $fastq_dir) circles=$(realpath $circles)"
 snakemakeconfig="$config $params $conda $cluster"
-snakemake --snakefile Snakefile --directory $workdir --configfile "genome.yaml" --config $snakemakeconfig --cluster "SlurmEasy -n {rule} -t {threads} -m 8G -l cluster_log"
+snakemake --snakefile Snakefile --directory $workdir --configfile "genome.yaml" --config $snakemakeconfig
